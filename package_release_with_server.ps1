9ca950378decc5425dfe7ff5d7ed6731bd04e222
Mihai <45673304+mihaicm93@users.noreply.github.com>
2024-04-23 14:38:57 +0200

    Update package_release_with_server.ps1

--- package_release_with_server.ps1
+++ package_release_with_server.ps1
@@ -59,6 +59,11 @@ if ($Commit.Length -gt 0) {
     $Commit = git rev-parse HEAD
 }
 
+$jsonFilePath = "./project/assets/configs/http.json"
+(Get-Content $jsonFilePath).Replace('127.0.0.1','0.0.0.0') | Set-Content $jsonFilePath
+# (Get-Content c:\temp\test.txt).Replace('[MYID]', 'MyValue') | Set-Content c:\temp\test.txt
+
+
 $packageJsonPath = "./project/package.json"
 $akiVer = (Get-Content $packageJsonPath -Raw | ConvertFrom-Json).version
 Write-Output "AKI_VERSION=$akiVer" >> "$env:GITHUB_OUTPUT"
